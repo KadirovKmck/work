3fe51d465867a25e47696f635573f03a99261464
kadirovMuhammadziz7 <kadirovmuhammadaziz79@gmail.com>
2023-10-24 12:55:16 +0600

    h

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:work_r/apps/views/register/main_home_register.dart';
 
-import 'apps/views/HomeView.dart';
-
 void main(List<String> args) {
   runApp(App());
 }
